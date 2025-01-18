d398e939690265ac4316a4fa22c86dd3eab3d2bf
Sujal Raj <rajsujal2004@gmail.com>
2025-01-18 12:57:37 +0530

    updated

---
+++ RandomImportantTopics/GCDEucledianAlgo.cpp
@@ -0,0 +1,15 @@
+// Eucledian Algorithm  =  T.C = O(logb)
+#include<iostream>
+using namespace std;
+int gcd (int a, int b){
+    if(a%b==0) {return b;}
+     return gcd(b,a%b);  // ex = gcd(2,5) is passed from main than this line will return (5,2%5) = (5, 2) -> correct seq
+}
+
+int main (){
+    int a,b;
+    cin>>a>>b;
+    
+    cout<<gcd(a,b); // standard = b<a but if b>a than also it will handle upr jaka reverse khud hojayaga
+}
+
--- week13_DivingintoMaths/countSetBits.cpp
+++ week13_DivingintoMaths/countSetBits.cpp
@@ -8,11 +8,11 @@ int count_Set_Bits2(int n){
     int count = 0;
     while(n>0){
         count++;
-        n = n & n-1;  //in each iteration one setbit will be removed fro n
+        n = n & n-1;  //in each iteration one setbit will be removed from n
     }
     return count;
 }
- //t.c -> no. od setbits
+ //t.c -> no. of setbits
 
 int main(){
     cout<<count_Set_Bits1(56)<<endl;
